52cd63153947707a8427feea629fe1fe26f4ea8d
Sachin Chavan <chavansachin295@gmail.com>
2019-06-23 02:05:36 +0530

    Add: production build gh pages

---
+++ precache-manifest.11b502101f7db26ff0ba77964c8f3d2e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2ddd9b86583ec1f9c6a0",
+    "url": "/js/chunk-vendors.b69ff4b4.js"
+  },
+  {
+    "revision": "34954041ce31c42f1f5d",
+    "url": "/js/app.670cd12d.js"
+  },
+  {
+    "revision": "2e5c2361db5b37e7dc1884fd239b76d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e87d0f326141a1a55d8ced930ed4d74",
+    "url": "/img/sachin_at_work.7e87d0f3.jpg"
+  },
+  {
+    "revision": "2320cf4ac8109854556771749a882ba5",
+    "url": "/img/Github-512.2320cf4a.png"
+  },
+  {
+    "revision": "1088ddfe4c35c8f344a96b4c42243b44",
+    "url": "/form.html"
+  },
+  {
+    "revision": "2ddd9b86583ec1f9c6a0",
+    "url": "/css/chunk-vendors.cdb08176.css"
+  },
+  {
+    "revision": "34954041ce31c42f1f5d",
+    "url": "/css/app.20bd0b68.css"
+  },
+  {
+    "revision": "f53a60e3e7ae1e6786b38c4206774628",
+    "url": "/_redirects"
+  }
+];
